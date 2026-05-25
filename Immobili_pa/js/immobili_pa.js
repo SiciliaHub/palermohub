44c8c292de863c143934c77c58e55873b6b05491
gbvitrano <gbvitrano@gmail.com>
2026-05-25 16:33:37 +0200

    Update immobili_pa.js

--- Immobili_pa/js/immobili_pa.js
+++ Immobili_pa/js/immobili_pa.js
@@ -1205,7 +1205,7 @@ function initMap() {
   const odsCtrl = L.control({ position: 'bottomright' });
   odsCtrl.onAdd = () => {
     const c = L.DomUtil.create('div', 'leaflet-control-ods');
-    c.innerHTML = `<a href="https://opendatasicilia.it/" target="_blank" rel="noopener" title="Open Data Sicilia"><img src="immobili_pa//img/opendatasicilia.png" alt="Open Data Sicilia" title="Open Data Sicilia"></a>`;
+    c.innerHTML = `<a href="https://opendatasicilia.it/" target="_blank" rel="noopener" title="Open Data Sicilia"><img src="immobili_pa/img/opendatasicilia.png" alt="Open Data Sicilia" title="Open Data Sicilia"></a>`;
     L.DomEvent.disableClickPropagation(c);
     return c;
   };
